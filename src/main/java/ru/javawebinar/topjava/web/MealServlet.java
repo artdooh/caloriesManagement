68e014bc3a6549f76985b4e01ac66935728fc68e
artdooh <dooh600rr@gmail.com>
2017-04-03 22:28:35 +0400

    HW1

---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,24 @@
+package ru.javawebinar.topjava.web;
+
+import ru.javawebinar.topjava.LoggerWrapper;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by тёма on 03.04.2017.
+ */
+public class MealServlet extends HttpServlet {
+    private static final LoggerWrapper LOG = LoggerWrapper.get(MealServlet.class);
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.debug("redirect to mealList");
+
+//        request.getRequestDispatcher("/mealList.jsp").forward(request, response);
+        response.sendRedirect("mealList.jsp");
+    }
+
+}
---
+++ src/main/webapp/mealList.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: тёма
+  Date: 03.04.2017
+  Time: 21:21
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>MealList</title>
+</head>
+<body>
+    <h3>MealList</h3>
+</body>
+</html>
