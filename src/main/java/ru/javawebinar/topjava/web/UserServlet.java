896cfc6c4502dd8eec2dc666ec2829b6e0d5958e
artdooh <dooh600rr@gmail.com>
2017-04-03 22:09:56 +0400

    1_6 1_7

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,16 +1,24 @@
 package ru.javawebinar.topjava.web;
 
+import ru.javawebinar.topjava.LoggerWrapper;
+
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * Created by тёма on 02.04.2017.
+ * User: gkislin
+ * Date: 19.08.2014
  */
-public class UserServlet extends javax.servlet.http.HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/userList.jsp").forward(req, resp);
+public class UserServlet extends HttpServlet {
+    private static final LoggerWrapper LOG = LoggerWrapper.get(UserServlet.class);
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.debug("redirect to userList");
+
+//        request.getRequestDispatcher("/userList.jsp").forward(request, response);
+        response.sendRedirect("userList.jsp");
     }
 }
